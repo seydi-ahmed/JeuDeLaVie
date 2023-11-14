5e82f949e8232a4ec638bf8f363edac66278ec18
mouhameddiouf <seydiahmedelcheikh@gmail.com>
2023-11-14 22:57:27 +0000

    Grille

--- game.py
+++ game.py
@@ -1,2 +1,13 @@
 import pygame
 import numpy as np
+
+# Dimensions de la grille
+WIDTH, HEIGHT = 800, 800
+ROWS, COLS = 40, 40
+
+# Couleurs
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+# Initialisation de la grille
+grid = np.zeros((ROWS, COLS))
